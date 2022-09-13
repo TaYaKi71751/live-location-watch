6561b613afed3309b048c5d8ec14550aa911bed8
TaYaKi71751 <shantian.tw@gmail.com>
2022-09-13 12:29:29 +0000

    Add `AddAuth`

--- src/index.js
+++ src/index.js
@@ -5,23 +5,39 @@ import reportWebVitals from './reportWebVitals';
 import {Subscriptions} from './util/Subscriptions';
 import {Config} from './util/Config';
 import {Device} from './util/Device';
+import {AddAuth} from './util/Auth';
+
 
 window.subscriptions = new Subscriptions();
 Config.auth.getAll().forEach((auth)=>{
 	window.subscriptions.connect(auth);
-})
+});
 
 const overlay = document.querySelector('.ol-overlaycontainer-stopevent');
+overlay.style.display = 'contents';
 overlay.style.width = 'fit-content';
 overlay.style.height = 'fit-content';
-overlay.style['pointer-events'] = 'unset';
-const root = ReactDOM.createRoot(overlay);
-root.render(
-	<>
-	{window.subscriptions.sockets.map((socket)=>(
+overlay.style['pointer-events'] = undefined;
+
+
+const top_left_overlay = document.querySelector('.ol-zoom.ol-unselectable.ol-control');
+top_left_overlay.className = '';
+top_left_overlay.style.position = 'absolute';
+top_left_overlay.style.width = 'fit-content';
+top_left_overlay.style.height = 'fit-content';
+top_left_overlay.style['pointer-events'] = undefined;
+
+
+const App = () => {
+	return <><AddAuth/>
+	<div style={{'overflow-x':'scroll',width:'min-content',height:'17vh'}} class="devices">{window.subscriptions.sockets.map((socket)=>(
 		<Device user={socket?.auth?.user} device={socket?.auth?.device}/>
-	))}
-	</>
+	))}</div>
+		</>
+}
+const root = ReactDOM.createRoot(top_left_overlay);
+root.render(
+	<App />
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/util/Device.js
+++ src/util/Device.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import {Config} from './Config';
+
+const js = JSON.stringify;
 /**
  * @param {{user:{email:string},device:{id:`${number}`}}} props 
  */
 export function Device(props){
  const user = props?.user;
  const device = props?.device;
- return <button className='device' id={device?.id}>
-		<button className='device info' id={device?.id}>
+ return <button style={{position:'relative',transform:'translateX(-50%)',left:'50%'}} className='device' id={device?.id}>
+		<button style={{display:'table'}} className='device info' id={device?.id}>
 			<div className='user email'>{user?.email}</div>
 			<div className='device id'>{device?.id}</div>
 			<div className='device location'></div>
 		</button>
-		<button onClick={()=>(Config.auth.delete({user,device}))}>🗑️</button>
-		<button className='device focus' onClick={()=>(
-			window.subscriptions.focus = `${window.subscriptions.focus}` === `${device?.id}` ? -1 : device?.id,
+		<button onClick={(e)=>{
+			Config.auth.delete({user,device});
+			window.subscriptions.sockets.forEach((socket)=>{
+				if(
+					js(socket.auth) === js({user,device})
+				){ socket.disconnect(); e.target.parentElement.remove(); }
+			})
+	 }}>🗑️</button>
+		<button className='device focus' onClick={()=>{
+			window.subscriptions.focus = `${window.subscriptions.focus}` === `${device?.id}` ? -1 : device?.id;
 			document.querySelectorAll(`[class="device"] > .focus`)
 				.forEach((focus)=>{
 					focus.textContent = (
 						`${window.subscriptions.focus}` === `${focus.parentElement.id}` ?'👀':'😌'
 					);
-				})
-		)}>{`${window.subscriptions.focus}` === `${device?.id}` ? '👀' : '😌'}</button>
+				});
+		}}>{`${window.subscriptions.focus}` === `${device?.id}` ? '👀' : '😌'}</button>
 		<button className='device place'>📍</button>
 	</button>;
 }
